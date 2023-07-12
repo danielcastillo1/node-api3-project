d54d2f92290e79d094998000ecaaa8cef4a96fb5
Daniel Castillo <danielcastillo89@icloud.com>
2023-07-12 16:36:20 -0500

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,28 +1,28 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
-  const timestamp = new Date().toLocaleString()
-  const method = req.method
-  const url = req.originalUrl
-  console.log(`[${timestamp}] ${method} to ${url}`)
-  next()
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} to ${url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validateUserId middleware")
-  next()
+  console.log("validateUserId middleware");
+  next();
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validateUser middleware")
-  next()
+  console.log("validateUser middleware");
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  console.log("validatePost middleware")
-  next()
+  console.log("validatePost middleware");
+  next();
 }
 
 // do not forget to expose these functions to other modules
@@ -30,5 +30,5 @@ module.exports = {
   logger,
   validateUserId,
   validateUser,
-  validatePost
-}
+  validatePost,
+};
